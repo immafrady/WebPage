447b89f5e753d2f3b4eb33a28a6b19a89fee8d13
Frady <immafrady@gmail.com>
2017-08-07 10:53:19 +0800

    Add BMI Caculator

---
+++ js/bmi.js
@@ -0,0 +1,14 @@
+// 建设中
+var h,w;
+var h =
+var bmi= w / (h * 2);
+function clickButton(){}
+if (bmi <= 18.5){{
+    document.getElementById('CNresult').innerHTML='食之无味，弃之可惜';
+} else if (bmi <=25) {
+
+} else if (bmi <=29.9) {
+
+} else {
+
+}}
