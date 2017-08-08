1257c089b0db43e00ba0690606cf78ea8806b264
Frady <immafrady@gmail.com>
2017-08-08 10:33:03 +0800

    Optimized. Empty value will return warning

--- js/bmi.js
+++ js/bmi.js
@@ -12,21 +12,31 @@ btn.onclick = function () {
         h = hm / 100,
         bmi = w / (h * h);
     document.getElementById('result').innerHTML = parseInt(bmi);
-    if (bmi <= 18.5) {
-        document.getElementById('CNresult').innerHTML = '食之无味，弃之可惜';
-        document.getElementById('JPresult').innerHTML = 'いっぱい食べてもいいぜ';
-        document.getElementById('ENresult').innerHTML = 'EAT or DIE';
-    } else if (bmi <= 25) {
-        document.getElementById('CNresult').innerHTML = '有点羡慕你了';
-        document.getElementById('JPresult').innerHTML = '羨ましい！！';
-        document.getElementById('ENresult').innerHTML = 'Doing Great!';
-    } else if (bmi <= 30) {
-        document.getElementById('CNresult').innerHTML = '你危险了';
-        document.getElementById('JPresult').innerHTML = '太くなっている';
-        document.getElementById('ENresult').innerHTML = 'Try to CONTROL yourself!';
-    } else {
-        document.getElementById('CNresult').innerHTML = '你是猪吗？';
-        document.getElementById('JPresult').innerHTML = 'てめえ、豚なのかよ！';
-        document.getElementById('ENresult').innerHTML = 'You taste so good';
+    // 加入switch语句
+    switch (isNaN(bmi)) {
+        case true:
+            document.getElementById('CNresult').innerHTML = '认真写，别逗我';
+            document.getElementById('JPresult').innerHTML = '冗談じゃない！';
+            document.getElementById('ENresult').innerHTML = 'What are you hidding?';
+            break;
+        default:
+        if (bmi <= 18.5) {
+            document.getElementById('CNresult').innerHTML = '食之无味，弃之可惜';
+            document.getElementById('JPresult').innerHTML = 'いっぱい食べてもいいぜ';
+            document.getElementById('ENresult').innerHTML = 'EAT or DIE';
+        } else if (bmi <= 25) {
+            document.getElementById('CNresult').innerHTML = '有点羡慕你了';
+            document.getElementById('JPresult').innerHTML = '羨ましい！！';
+            document.getElementById('ENresult').innerHTML = 'Doing Great!';
+        } else if (bmi <= 30) {
+            document.getElementById('CNresult').innerHTML = '你危险了';
+            document.getElementById('JPresult').innerHTML = '太くなっている';
+            document.getElementById('ENresult').innerHTML = 'Try to CONTROL yourself!';
+        } else {
+            document.getElementById('CNresult').innerHTML = '你是猪吗？';
+            document.getElementById('JPresult').innerHTML = 'てめえ、豚なのかよ！';
+            document.getElementById('ENresult').innerHTML = 'You taste so good';
+        }
     }
+
 }
