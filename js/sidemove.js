d64347c3c6f5fb17a8ec6a217b05d12f9911b720
Frady <immafrady@gmail.com>
2017-08-18 17:56:32 +0800

    make sidebar move

--- js/sidemove.js
+++ js/sidemove.js
@@ -1,1 +1,57 @@
- 
+var leftCol = document.getElementById('leftColumn');
+var rightCol = document.getElementById('rightColumn');
+var ogLeft = -190;
+var ogRight = -190;
+var timerL;
+var timerR;
+
+// 左边
+leftCol.onmouseover = function(){
+    clearInterval(timerL);
+
+    timerL = setInterval(function() {
+        if(ogLeft >= -100){
+            clearInterval(timerL)
+        } else{
+            ogLeft += 10;
+        }
+        leftCol.style.left = ogLeft + 'px';
+    }, 20) 
+}
+leftCol.onmouseout = function(){
+    clearInterval(timerL);
+
+    timerL = setInterval(function(){
+        if(ogLeft <= -190){
+            clearInterval(timerL)
+        } else {
+            ogLeft -= 10;
+        }
+        leftCol.style.left = ogLeft + 'px';
+    },20)
+}
+
+// 右边
+rightCol.onmouseover = function(){
+    clearInterval(timerR);
+    timerR = setInterval(function(){
+        if(ogRight>=-100){
+            clearInterval(timerR)
+        } else {
+                ogRight+=10;
+        }
+    rightCol.style.right = ogRight + 'px';
+    },20);
+}
+rightCol.onmouseout = function(){
+    clearInterval(timerR);
+
+    timerR = setInterval(function(){
+        if(ogRight <= -190){
+            clearInterval(timerR)
+        }else{
+            ogRight-= 10;
+        }
+        rightCol.style.right = ogRight + 'px';
+    },20)
+}
